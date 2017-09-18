7ec49aa881629e19c2d02f4271136600e5c8f54c
Alexander Blom <blal16@nti.umea>
2017-09-18 10:36:10 +0200

    Program som gör ett ljud

---
+++ Ovning1_7.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*
+	Alexander Blom
+	Program som gör ett ljud
+	2017/09/18
+*/
+
+public class Ovning1_7 {
+	public static void main(String[] args) {
+		Toolkit.getDefaultToolkit().beep();
+	}
+}
