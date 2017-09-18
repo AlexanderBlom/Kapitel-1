81d2e9d1f07558b349dd15722e3a4d1985b8d508
Alexander Blom <blal16@nti.umea>
2017-09-18 10:20:42 +0200

    Program som skriver mitt namn och nummer

---
+++ Ovning1_6.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+/*
+ Vad?
+ Vem?
+ När?
+*/
+
+public class Ovning1_6 {
+	public static void main (String[] arg){
+		JOptionPane.showMessageDialog(null,"Alexander Blom");
+		JOptionPane.showMessageDialog(null,"076 779 75 33");
+	}
+}
+
