6e1dba61a45bc80e4f63cf38599a6c699132e6e5
Alexander Blom <blal16@nti.umea>
2017-09-18 10:57:59 +0200

    Övning 1.9

---
+++ Ovning1_9.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/*
+	Alexander Blom
+	Program som skriver mitt namn, adress och nummer på tre rader.
+	2017/09/18
+*/
+
+public class Ovning1_9{
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Alexander Blom" + 
+			"\nN Drottninggatan 40" +
+			"\n076 779 75 33");
+	}
+}
